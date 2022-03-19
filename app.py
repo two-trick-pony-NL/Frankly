ce9bdbaf1fc063640e145ac0a72566015e884ac6
Peter van Doorn <peter@petervandoorn.com>
2022-03-19 22:32:53 +0100

    cleanup added comments

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ from sqlalchemy import true
 from website import create_app
 
 app = create_app()
+#This line is disabled so that Gunicorn can take port 80 to expose to the public. 
 #app.run(debug=True, host='0.0.0.0', port=80)
