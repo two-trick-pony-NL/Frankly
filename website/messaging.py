c3d47a110e445c33c5fb9ec1a51bca57cfac7346
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-03-28 21:23:12 +0200

    Addes spacing and Auth to whatsapp

--- website/messaging.py
+++ website/messaging.py
@@ -1,6 +1,7 @@
 from curses import flash
 import pyshorteners
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, session
+from flask_login import login_required, current_user
 from configparser import ConfigParser
 from twilio.rest import Client 
 
@@ -25,12 +26,13 @@ print("The Shortened URL is: " + short_url)
 
 #From here we have the Twillio Whatsapp integration
 @messaging.route("/sendwhatsapp/<userid>/<phonenumber>")
+@login_required
 def SendWhatsapp(userid, phonenumber):
     client = Client(account_sid, auth_token) 
     phonenumber = str(phonenumber)
-    promotorURL = str('\n😃 great! > https://grapevine.works/send-feedback/'+userid+'/3')
-    neutralURL = str('\n😑 mehh! > https://grapevine.works/send-feedback/'+userid+'/2')
-    detractorURL = str('\n😢 Not so good > https://grapevine.works/send-feedback/'+userid+'/1')
+    promotorURL = str('\n\n\n😃 great! > https://grapevine.works/send-feedback/'+userid+'/3')
+    neutralURL = str('\n\n\n😑 mehh! > https://grapevine.works/send-feedback/'+userid+'/2')
+    detractorURL = str('\n\n\n😢 Not so good > https://grapevine.works/send-feedback/'+userid+'/1')
     message = 'Thanks, what did you think of the workshop?'
     print(userid)
     
