d2bfaacfcd85ac89b0c22ed998ebebd1014a36d8
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-04-04 15:16:52 +0200

    extended the number of characters allowed on post

--- website/models.py
+++ website/models.py
@@ -50,7 +50,7 @@ class Post(db.Model):
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.String(1000), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
