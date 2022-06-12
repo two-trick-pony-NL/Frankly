99c33191fc38577820d347b33c20fd9809634105
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-06-12 17:39:09 +0200

    login test in playwright added

--- test/playwright/test-login.py
+++ test/playwright/test-login.py
@@ -4,7 +4,7 @@ import time
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
-    time.sleep( 5)
+    time.sleep(5)
     # Open new page
     page = context.new_page()
     # Go to http://localhost:8080/
@@ -15,7 +15,7 @@ def run(playwright: Playwright) -> None:
     # Click [placeholder="Enter your Email"]
     page.locator("[placeholder=\"Enter your Email\"]").click()
     # Fill [placeholder="Enter your Email"]
-    page.locator("[placeholder=\"Enter your Email\"]").fill("test@testuser2022.com")
+    page.locator("[placeholder=\"Enter your Email\"]").fill("testuser2022@petervandoorn.com")
     # Press Tab
     page.locator("[placeholder=\"Enter your Email\"]").press("Tab")
     # Fill [placeholder="Enter your Password"]
@@ -24,27 +24,10 @@ def run(playwright: Playwright) -> None:
     page.locator("section:has-text(\"Sign In to your account Email Address Password If you have forgotten your passwo\")").click()
     # Click text=Login
     page.locator("text=Login").click()
-     expect(page).to_have_url("http://localhost:8080/sign-in")
-    # Click [placeholder="Enter your Email"]
-    page.locator("[placeholder=\"Enter your Email\"]").click()
-    
-    # Click text=Login
-    page.locator("text=Login").click()
-     expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?user=%3CUser+2%3E")
-    # Click a:has-text("2") >> nth=0
-    page.locator("a:has-text(\"2\")").first.click()
-     expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?page=2")
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
+    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?user=%3CUser+59%3E")
     # Click text=Log Out
     page.locator("text=Log Out").click()
-     expect(page).to_have_url("http://localhost:8080/home")
+    # expect(page).to_have_url("http://localhost:8080/home")
     # ---------------------
     context.close()
     browser.close()
