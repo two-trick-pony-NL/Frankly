67f6ddebaeb5f861fc70d29f92ec2e52d9f17dad
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-06-12 18:10:26 +0200

    Added shortname to CI file and  dashboard test

--- test/playwright/test-dashboard.py
+++ test/playwright/test-dashboard.py
@@ -37,60 +37,18 @@ def run(playwright: Playwright) -> None:
     page.locator("[placeholder=\"bob\\@bob\\.com\\, alice\\@alice\\.com\"]").fill("test1@petrervandoorn.com, test2@petrervandoorn.com")
     # Click text=Send to client
     page.locator("text=Send to client").click()
-    time.sleep(5)
+    time.sleep(2)
+    page.goto("http://localhost:8080/dashboard/testuser2022")
+    time.sleep(2)
+    # Click text=Next Next graph
+    page.locator("text=Next Next graph").click()
+    # Click text=Next Next graph
+    page.locator("text=Next Next graph").click()
+    # Click text=Next Next graph
+    page.locator("text=Next Next graph").click()
+    # Click text=Next Next graph
+    page.locator("text=Next Next graph").click()
     
-    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?user=%3CUser+59%3E")
-    # Click .offcanvas-backdrop
-    page.locator(".offcanvas-backdrop").click()
-    # Click a:has-text("2") >> nth=0
-    page.locator("a:has-text(\"2\")").first.click()
-    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?page=2")
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click text=Next Next graph
-    page.locator("text=Next Next graph").click()
-    # Click a:has-text("Settings")
-    page.locator("a:has-text(\"Settings\")").click()
-    # Click text=Remaining Credits
-    page.locator("text=Remaining Credits").click()
-    # Click text=Survey Settings
-    page.locator("text=Survey Settings").click()
-    # Click [placeholder="How did you like our service\?"]
-    page.locator("[placeholder=\"How did you like our service\\?\"]").click()
-    # Fill [placeholder="How did you like our service\?"]
-    page.locator("[placeholder=\"How did you like our service\\?\"]").fill("How did you like the service?")
-    # Click text=Save Custom Questions
-    page.locator("text=Save Custom Questions").click()
-    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?page=2")
-    # Click a:has-text("Settings")
-    page.locator("a:has-text(\"Settings\")").click()
-    # Click text=Survey Settings
-    page.locator("text=Survey Settings").click()
-    # Click [placeholder="How did you like the service\?"]
-    page.locator("[placeholder=\"How did you like the service\\?\"]").click()
-    # Click [placeholder="What do you think we can improve\?"]
-    page.locator("[placeholder=\"What do you think we can improve\\?\"]").click()
-    # Fill [placeholder="What do you think we can improve\?"]
-    page.locator("[placeholder=\"What do you think we can improve\\?\"]").fill("How do you think we can improve?")
-    # Click text=Save Custom Questions
-    page.locator("text=Save Custom Questions").click()
-    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?page=2")
-    # Click a:has-text("Settings")
-    page.locator("a:has-text(\"Settings\")").click()
-    # Click text=Survey Settings
-    page.locator("text=Survey Settings").click()
-    # Click [placeholder="is there anything else you\'d like to tell us\?"]
-    page.locator("[placeholder=\"is there anything else you\\'d like to tell us\\?\"]").click()
-    # Fill [placeholder="is there anything else you\'d like to tell us\?"]
-    page.locator("[placeholder=\"is there anything else you\\'d like to tell us\\?\"]").fill("is there anything else you'd like to tell us? ")
-    # Click text=Save Custom Questions
-    page.locator("text=Save Custom Questions").click()
-    # expect(page).to_have_url("http://localhost:8080/dashboard/testuser2022?page=2")
-    # Click text=Log Out
     page.locator("text=Log Out").click()
     # expect(page).to_have_url("http://localhost:8080/home")
     # ---------------------
